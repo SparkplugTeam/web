2e497b8b73ddb797a141e50482c22be9a81ea9ad
Matt C <mcompton2002@gmail.com>
2021-02-24 22:20:22 -0500

    heeho

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from jinja2 import Template
+
+import os
+
+def get_template(name):
+    with open("templates" + os.path.sep + name) as f:
+        return f.read() 
+
+main = Template(get_template("page.html"))
+
+app = FastAPI(default_response_class=HTMLResponse)
+
+@app.get("/")
+async def root():
+    return main.render(page="Home", content="Heeho")

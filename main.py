85e4a1b061414dc0d4c65de136b276fe04cf14f1
Matt C <mcompton2002@gmail.com>
2021-02-25 00:21:56 -0500

    Sanic instead of flask

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import os
 
 # Pip modules
-from flask import *
+from sanic import Sanic
+from sanic import response
+from jinja2 import Template
 import requests
 
 # our fuckery
@@ -10,29 +12,38 @@ from datatypes.User import User
 from datatypes.Post import Post
 from datatypes.Category import Category
 
+
+def template_html(name):
+    return open("templates" + os.path.sep + name + ".html").read()
+
+
+# Templates (for now)
+main_template = Template(template_html("page"))
+
 JSERV_URL = "http://localhost:4040"
 API_KEY = "SparkPlug"
 DB_NAME = "SparkPlug"
 
 JSERV_HEADERS = {"x-api-key": API_KEY}
 
-app = Flask(__name__)
+app = Sanic(name=__name__)
 
 
 @app.route("/")
-def root():
+async def root(request):
     u = User("Yeet", "yoink", "hee@hoo.com")
     c = Category("aa", "aaaaaaaa", u)
     p = Post(u, c, "fuck", "shitass")
-    return render_template("page.html", page="Home", content=p.renderPost())
+    return response.html(main_template.render(page="Home", content=p.renderPost()))
 
 
 @app.route("/jserv/new")
-def newobj():
+async def newobj(request):
     nid = requests.request(
         "GET", JSERV_URL + "/query/newId?q=" + DB_NAME, headers=JSERV_HEADERS, data={}
     ).text
-    return nid
+    return response.text(nid)
 
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
